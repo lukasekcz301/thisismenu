d0c202f2eadcc981b3602da5180d1b9fa57e1cd1
lukasekcz301 <lukasekcz301@gmail.com>
2026-09-12 10:08:03 +0200

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,6 @@
+local id = game.PlaceId
+if id == 6516141723 then
+	loadstring(game:HttpGet("http://raw.githubusercontent.com/lukasekcz301/thisismenu/refs/heads/main/lobby.lua"))()
+elseif id == 6839171747 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/lukasekcz301/thisismenu/refs/heads/main/thehotel.lua"))()
+end
